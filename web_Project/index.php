c3e7bc5c6b0af59988c2197a179d1ad233f431cb
hamdanalthafif <112264220+hamdanalthafif@users.noreply.github.com>
2024-02-10 00:06:45 +0300

    Update and rename index to index.php

---
+++ web_Project/index.php
@@ -0,0 +1,18 @@
+
+<html>
+    <?php
+    include("header.php");
+    ?>
+    <body>
+         <?php
+    include("body.php");
+    ?>
+    <footer>
+        <?php
+    include("Footer.php");
+    ?> 
+    </footer>
+    
+    </body>
+    
+</html>
